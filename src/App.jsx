967722049448c8916bf693a5d88b93ad0164a688
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-04 10:30:21 +0600

    recart implement

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
+import Chart from './Components/ResultCharts/Chart';
 
 const App = () => {
   return (
     <div>
       <nav>
         <Navbar></Navbar>
+        <Chart></Chart>
       </nav>
     </div>
   );
---
+++ src/Components/ResultCharts/Chart.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { LineChart } from 'recharts';
+
+const Chart = () => {
+    return (
+        <div>
+            <LineChart width={500}height={500}>
+
+                
+            </LineChart>
+        </div>
+    );
+};
+
+export default Chart;
