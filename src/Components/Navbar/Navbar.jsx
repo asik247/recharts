8f9c1ecabeba08813a70f030438ba26c4d0be003
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-04 10:07:08 +0600

    nav code done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,33 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Navbar = () => {
-    return (
-     <div>
-        <div className="navContainer flex justify-between items-center w-11/12 mx-auto mt-10">
-            <div className="navStart flex items-center">
 
-                  <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div>
+
+            <div className="navContainer flex justify-between items-center w-11/12 mx-auto mt-10 relative">
+
+                <div className="navStart flex items-center gap-3">
+
+                    <div
+                        className="lg:hidden relative"
+                        onMouseEnter={() => setIsOpen(true)}
+                        onMouseLeave={() => setIsOpen(false)}
+                    >
+                        <button className="btn btn-ghost">
+                            ☰
+                        </button>
+
+                        {isOpen && (
+                            <ul className="absolute top-full left-0 bg-white shadow-md rounded w-40">
+                                <li className="p-2 hover:bg-gray-200 cursor-pointer">Home</li>
+                                <li className="p-2 hover:bg-gray-200 cursor-pointer">About</li>
+                                <li className="p-2 hover:bg-gray-200 cursor-pointer">Profile</li>
+                                <li className="p-2 hover:bg-gray-200 cursor-pointer">Contact</li>
+                            </ul>
+                        )}
                     </div>
                     <div className="logo">
-                        <h1>Rechart</h1>
+                        <h1 className="text-xl font-bold">Rechart</h1>
                     </div>
 
-            </div>
-            <div className="navCenter hidden lg:flex">
-                <ul className='flex gap-4 items-center'>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Profile</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </div>
-            <div className="navEnd">
-                <button className='btn btn-ghost'>LogOut</button>
-                <button className='btn btn-ghost'>LogOut</button>
+                </div>
+
+                <div className="navCenter hidden lg:flex">
+                    <ul className="flex gap-6 items-center">
+                        <li className="hover:text-blue-500 cursor-pointer">Home</li>
+                        <li className="hover:text-blue-500 cursor-pointer">About</li>
+                        <li className="hover:text-blue-500 cursor-pointer">Profile</li>
+                        <li className="hover:text-blue-500 cursor-pointer">Contact</li>
+                    </ul>
+                </div>
+
+                <div className="navEnd">
+                    <button className="btn btn-ghost">LogOut</button>
+                </div>
+
             </div>
         </div>
-     </div>
     );
 };
 
